1bdf27e90f043377ca00bb755f5432a1d7295993
DhruvJansari <dhruvjansari281@gmail.com>
2026-06-06 12:09:23 +0530

    feat: implement isValid function to check for valid parentheses

--- Question2.js
+++ Question2.js
@@ -1,3 +1,26 @@
-str1 = "()";
-str2 = "()[]{}";
-str3 = "([)]";
+function isValid(s) {
+  if (s.length % 2 !== 0) return false;
+
+  const stack = [];
+  const map = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (map[char]) {
+      const topElement = stack.length > 0 ? stack.pop() : "#";
+
+      if (topElement !== map[char]) {
+        return false;
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
+}
